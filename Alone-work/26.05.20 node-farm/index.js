77bf9bd03291c2f6c533ebc8a81892b9acefa4e1
Simona <see.moona@yahoo.com>
2020-05-26 18:30:14 +0200

    node farm: node intro

---
+++ Alone-work/26.05.20 node-farm/index.js
@@ -0,0 +1,69 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+/////////////////////////////////
+//Files
+
+//blocking synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
+
+// const textOut = `This is what we know about the avocado ${textIn}.\n Created on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File updated");
+
+//unblocking asynchronous way || Write in Callback HELL
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//     if (err) return console.log('ERROR! 💣')
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
+
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+//         console.log("Your file has been written");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file");
+
+////////////////////////////////////
+/////SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  //Overview page
+
+  if (pathName === "/" || pathName === "/overview") {
+
+
+    res.end("This is the OVERVIEW");
+
+    //Product page
+  } else if (pathName === "/PRODUCT") {
+    res.end("This is the product");
+
+    //API
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+
+    //NOt FOUND
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>The page was not found!<h1>");
+  }
+
+  res.end("Hello from the server!");
+});
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
