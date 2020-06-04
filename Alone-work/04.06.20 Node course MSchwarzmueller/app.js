b61f7bc3d9fc0c082478a8816ba6c176bdbf14b3
Simona <see.moona@yahoo.com>
2020-06-04 16:10:35 +0200

    req, res, scripts, .setHeader

--- Alone-work/04.06.20 Node course MSchwarzmueller/app.js
+++ Alone-work/04.06.20 Node course MSchwarzmueller/app.js
@@ -1,44 +1,9 @@
-const http = require("http");
-const fs = require("fs");
-const server = http.createServer((req, res) => {
-  //  console.log(req.url, req.method, req.headers);
-  // process.exit();
-  const url = req.url;
-  const method = req.method;
+const http = require('http');
 
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My first Page</title></head>");
-    res.write(
-      '<body><form action = "/message" method = "POST"><input type ="text" name="message"><button type = "submit"> Send </button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
+const routes = require('./routes');
 
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first Page</title></head>");
-  res.write("<body><h1>Hello from my first Node.js Page</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
